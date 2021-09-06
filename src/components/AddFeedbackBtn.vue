2fe70ca694b7950a6a3d4417e037e2bb496f4432
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-09-05 23:28:27 -0300

    component that direct user from screen creation feedback

---
+++ src/components/AddFeedbackBtn.vue
@@ -0,0 +1,24 @@
+<template>
+  <button>+ Add Fedback</button>
+</template>
+
+<script></script>
+<style scoped>
+button {
+  color: var(--light-gray);
+  font-size: 0.8125em;
+  font-weight: 700;
+  border: none;
+  padding: 0.807em 1em;
+  border-radius: 10px;
+  border: 1px solid transparent;
+  cursor: pointer;
+  transition: all 300ms ease-in-out 0s;
+  min-width: 134px;
+  height: 40px;
+  background-color: var(--purple-color);
+}
+button:hover {
+  background-color: rgb(199, 90, 246);
+}
+</style>
