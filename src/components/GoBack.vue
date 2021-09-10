adb5c4c2dc46157d2b8951d1efe4535aa6a9e321
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-09-09 22:56:28 -0300

    crete GoBack component

---
+++ src/components/GoBack.vue
@@ -0,0 +1,37 @@
+<template>
+  <div @click="onPressed">
+    <img src="../assets/arrow-left.svg" alt="" />
+    <a>Go back</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoBack",
+  props: ["onPressed"],
+};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  align-items: center;
+}
+
+div img {
+  width: 9px;
+  height: 9px;
+}
+
+div a {
+  color: var(--grayish-blue);
+  font-weight: bold;
+  font-size: 0.8125em;
+  margin-left: 1.153em;
+  transition: all 300ms ease-in-out 0s;
+  text-decoration: none;
+}
+div a:hover {
+  text-decoration: underline;
+}
+</style>
