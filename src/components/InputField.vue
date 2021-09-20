b087ae9f4c7315317f8a9a0adb2f547568163997
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-09-19 23:54:03 -0300

    create new component

---
+++ src/components/InputField.vue
@@ -0,0 +1,38 @@
+<template>
+  <input v-if="element == 'input'" name="" id="" />
+  <textarea v-else />
+</template>
+
+<script>
+export default {
+  name: "inputField",
+  props: ["element"],
+};
+</script>
+<style scoped>
+input {
+  width: 100%;
+  height: 48px;
+  border-radius: 5px;
+  background-color: var(--background-color);
+  transition: all 300ms ease-in-out 0s;
+  color: var(--dark-blue);
+  padding: 0px 1.5em;
+  outline: none;
+}
+
+input:hover,
+input:focus,
+textarea:hover,
+textarea:focus {
+  border-color: var(--light-blue);
+  outline: var(--light-blue);
+}
+
+textarea {
+  padding: 1.5em;
+  height: 150px;
+  resize: none;
+  margin-bottom: 1.23em;
+}
+</style>
