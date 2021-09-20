b20361690aeb5a065d91df1c875f42053b18da75
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-09-19 22:52:16 -0300

    removed comment

--- src/lib/defaultFeatures.js
+++ src/lib/defaultFeatures.js
@@ -1,9 +1,1 @@
 export const defaultFeatures = ["UI", "UX", "Enchancement", "Bug", "Feature"];
-
-{
-  /* <button class="filter_option" v-bind:class="[active]">UI</button>
-<button class="filter_option">UX</button>
-<button class="filter_option">Enchancement</button>
-<button class="filter_option">Bug</button>
-<button class="filter_option">Feature</button> */
-}
