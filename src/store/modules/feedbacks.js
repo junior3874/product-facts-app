9ed83561b08527a5fa74a1537dc787d316f13b6a
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-09-28 01:39:23 -0300

    implemented action for create new comment and created new mutation for feedbacks state

--- src/store/modules/feedbacks.js
+++ src/store/modules/feedbacks.js
@@ -1,4 +1,9 @@
-import requestContractWithAxios from "../../services/RequestToolWithAxios.js";
+/* eslint-disable no-unused-vars */
+
+import makeIndexFeedbackController from "../../factories/makeIndexFeedbackController.js";
+import makePostFeedbackCommentController from "../../factories/makePostFeedbackCommentController.js";
+
+// states
 const state = () => ({
   feedbacks: [],
 });
@@ -8,7 +13,7 @@ const getters = {
   getAllFeedbacksWithStatus: (state) => (key) =>
     state.feedbacks.filter((feedback) => feedback.status === key),
   showFeedbackWithId: (state) => (id) => {
-    const response = state.feedbacks.filter((feedback) => feedback.id == id);
+    const response = state.feedbacks.find((feedback) => feedback.id == id);
     return response;
   },
 };
@@ -16,8 +21,29 @@ const getters = {
 // actions
 const actions = {
   async getAllfeedbacks({ commit }) {
-    const response = await requestContractWithAxios.onGetAllFeedbacks();
-    commit("setFeedbacks", response.productRequests);
+    const indexFeedbackController = makeIndexFeedbackController();
+    const response = await indexFeedbackController.handler();
+
+    commit("setFeedbacks", response.feedbacks);
+  },
+  async postFeedbackCommentController({ commit, rootState }, payload) {
+    const { currentUser } = rootState.authUser;
+    if (!currentUser) {
+      return;
+    }
+
+    const postFeedbackCommentController = makePostFeedbackCommentController();
+
+    const response = await postFeedbackCommentController.handler({
+      ...payload,
+      userProps: currentUser,
+    });
+    if (response.error) {
+      return response;
+    }
+
+    commit("createComment", response.feedback);
+    return response;
   },
 };
 
@@ -25,6 +51,15 @@ const actions = {
 const mutations = {
   setFeedbacks(state, feedbacks) {
     state.feedbacks = feedbacks;
+  },
+  createComment(state, feedback) {
+    const alteredFeedback = state.feedbacks.map((element) => {
+      if (element.id == feedback.id) {
+        element = feedback;
+      }
+      return element;
+    });
+    state.feedbacks = alteredFeedback;
   },
 };
 
