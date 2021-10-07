4f6a0311fee7aab237e4713996956b1e4a8014e4
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-10-07 16:28:13 -0300

    removed facotires

--- src/factories/makeIndexFeedbackController.js
+++
@@ -1,9 +0,0 @@
-import IndexFeedbackController from "../controllers/feedbacks/indexFeedbackController";
-import IndexFeedbacksWithAxios from "../infra/services/indexFeedbackWithAxios";
-
-function makeIndexFeedbackControllerWithIndexAxiosService() {
-  const indexFeedbackWithAxios = new IndexFeedbacksWithAxios();
-  return new IndexFeedbackController(indexFeedbackWithAxios);
-}
-
-export default makeIndexFeedbackControllerWithIndexAxiosService;
--- src/factories/makeShowFeedbackController.js
+++
@@ -1,9 +0,0 @@
-import ShowFeedbackController from "../controllers/feedbacks/showFeedbackController";
-import ShowFeedbacksWithAxios from "../infra/services/showFeedbackWithAxios";
-
-function makeShowFeedbackControllerWithShowAxiosService() {
-  const showFeedbacksWithAxios = new ShowFeedbacksWithAxios();
-  return new ShowFeedbackController(showFeedbacksWithAxios);
-}
-
-export default makeShowFeedbackControllerWithShowAxiosService;
