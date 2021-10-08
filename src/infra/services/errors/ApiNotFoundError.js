1b324e549927d2d9cc10c75c72ee5f5b89d091e5
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-10-07 23:02:06 -0300

    removed services

--- src/infra/services/errors/ApiNotFoundError.js
+++
@@ -1,6 +0,0 @@
-export default class ApiNotFoundError extends Error {
-  constructor() {
-    const message = "Server is inactive";
-    super(message);
-  }
-}
--- src/infra/services/errors/FeedbackNotFoundError.js
+++
@@ -1,6 +0,0 @@
-export default class FeedbackNotFound extends Error {
-  constructor() {
-    const message = "Feedback not found";
-    super(message);
-  }
-}
