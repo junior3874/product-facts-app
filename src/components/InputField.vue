4dbe03f4b97745d3c4e750819e8ddc9e0377296c
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-10-21 01:53:35 -0300

    Update this component for use TS and added section error, that poop with one fn

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,17 +1,45 @@
 <template>
-  <input ref="inputField" v-if="typeElement == 'input'" />
-  <textarea v-else />
+  <div class="content-input-field">
+    <input
+      v-if="typeElement == 'input'"
+      :class="{ error: fieldErrorMessageVisible }"
+    />
+    <input
+      v-else-if="typeElement == 'input-readonly'"
+      :value="value"
+      :class="{ error: fieldErrorMessageVisible }"
+      readonly
+    />
+
+    <textarea v-else :class="{ error: fieldErrorMessageVisible }" />
+    <span :class="{ error: fieldErrorMessageVisible }">{{ messageError }}</span>
+  </div>
 </template>
 
-<script>
-import commentConstants from "../lib/CommentConsts.js";
+<script lang="ts">
+import { defineComponent } from "vue";
 
-export default {
+export default defineComponent({
   name: "InputField",
-  props: ["typeElement", "limit"],
-};
+  props: ["typeElement", "limit", "messageError", "value", "onPressed"],
+  data() {
+    return {
+      fieldErrorMessageVisible: false,
+    };
+  },
+  methods: {
+    toggleFieldError(status: boolean) {
+      this.fieldErrorMessageVisible = status;
+    },
+  },
+});
 </script>
 <style scoped>
+.content-input-field {
+  margin-bottom: 0px !important;
+  cursor: pointer;
+  position: relative;
+}
 input,
 textarea {
   width: 100%;
@@ -24,6 +52,14 @@ textarea {
   outline: none;
   border: 1px solid transparent;
   font-size: 0.8125em;
+  position: relative;
+}
+
+input.error,
+textarea.error {
+  border-color: red !important;
+
+  transition: none;
 }
 
 input:hover,
@@ -38,6 +74,20 @@ textarea {
   padding: 1.5em;
   height: 100px;
   resize: none;
-  margin-bottom: 1.23em;
+}
+
+span {
+  margin: 5px 0;
+  visibility: hidden;
+  font-size: 0.8rem;
+  position: inherit;
+  color: rgb(255, 90, 90);
+
+  position: absolute;
+  left: 0;
+  bottom: -50%;
+}
+span.error {
+  visibility: visible;
 }
 </style>
